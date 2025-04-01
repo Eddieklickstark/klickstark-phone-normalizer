fae114b8d17f787b3e6aa8bebc2e55d38d3873cb
Eddieklickstark <eddie.esche@digital-act.de>
2025-04-02 00:28:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,5 @@ app.get('/', (req, res) => {
   res.send('Phone Normalizer API is running. POST to /api/normalize with { phone, country }');
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
